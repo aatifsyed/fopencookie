3dbb54bbe773adecb7b4024a8f1ba3c39c663b10
Aatif Syed <aatifsyedyp@gmail.com>
2024-05-10 09:59:37 +0100

    Revert "wip: fdio"
    This reverts commit bdf0bdde92b060ee89b9b6356c8b0983687e485b.

--- fdio/src/lib.rs
+++ fdio/src/lib.rs
@@ -1,48 +1,14 @@
-use std::io;
-use std::os::fd::{AsFd, AsRawFd};
-
-use libc::c_void;
-
-pub struct FdIo<T>(pub T);
-
-impl<T: AsFd> io::Write for FdIo<T> {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        unsafe {
-            libc::write(
-                self.0.as_fd().as_raw_fd(),
-                buf.as_ptr().cast::<c_void>(),
-                buf.len(),
-            )
-        }
-        .try_into()
-        .map_err(|_| io::Error::last_os_error())
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        match unsafe { libc::fsync(self.0.as_fd().as_raw_fd()) } {
-            0 => Ok(()),
-            -1 => Err(io::Error::last_os_error()),
-            _undocumented => Err(io::Error::last_os_error()),
-        }
-    }
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
 }
 
-impl<T: AsFd> io::Read for FdIo<T> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        unsafe {
-            libc::read(
-                self.0.as_fd().as_raw_fd(),
-                buf.as_mut_ptr().cast::<c_void>(),
-                buf.len(),
-            )
-        }
-        .try_into()
-        .map_err(|_| io::Error::last_os_error())
-    }
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-impl<T: AsFd> io::Seek for FdIo<T> {
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        todo!()
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
     }
 }
